1dd6b037d5923e294003d8e2a9df0c2eeeebbcbb
Ann Akech <akechannatong@gmail.com>
2023-02-02 12:39:55 +0300

    variable

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    var fullname = "Atong"
+
+    var age = 20
+    var weightInKg = 54
+    var phoneNumber = "0793985100"
+    var Kenyan: Boolean = false
+    var kenyan: Boolean = true
+    println(age)
+
+
+
+
+
+
+
+
+}
